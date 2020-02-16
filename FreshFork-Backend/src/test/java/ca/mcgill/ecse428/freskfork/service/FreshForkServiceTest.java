103fa1c9566c1c409f1d658dbba69a1e11fb6f8b
Ashish Paul <apaul@m3-tech.ca>
2020-02-16 17:22:13 -0500

    added test class

---
+++ FreshFork-Backend/src/test/java/ca/mcgill/ecse428/freskfork/service/FreshForkServiceTest.java
@@ -0,0 +1,28 @@
+package ca.mcgill.ecse428.freskfork.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ca.mcgill.ecse428.freskfork.dao.*;
+import ca.mcgill.ecse428.freskfork.model.*;
+
+
+public class FreshForkServiceTest {
+	
+	
+
+}
